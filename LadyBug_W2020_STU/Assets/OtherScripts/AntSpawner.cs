57b9e72776fb431ece25d50bfd9e60f98b9a2e55
stamayolosarcos <stamayoi@eud.tecnocampus.cat>
2020-03-12 18:34:55 +0100

    Las hormigas spawnean quietas

--- LadyBug_W2020_STU/Assets/OtherScripts/AntSpawner.cs
+++ LadyBug_W2020_STU/Assets/OtherScripts/AntSpawner.cs
@@ -4,17 +4,43 @@ using UnityEngine;
 
 public class AntSpawner : MonoBehaviour {
 
-	
+    float elapsedTime = 0f;
+    float seedAverage = 0.8f;
+    float average;
+
+    public float timeToSpawn = 15f;
+    public GameObject eggAnt;
+    public GameObject seedAnt;
 
 	// Use this for initialization
 	void Start () {
-		// get the prefabs
-		
+        // get the prefabs
+        
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+        if (elapsedTime < timeToSpawn)
+        {
+            elapsedTime += Time.deltaTime;
+        }
+        else
+        {
+            average = Random.Range(0f, 1f);
+
+            if (average <= seedAverage)
+            {
+                Instantiate(seedAnt, this.transform.position, this.transform.rotation);                
+                Debug.Log("Spawn Seed Ant");
+            }
+            else
+            {
+                Instantiate(eggAnt, this.transform.position, this.transform.rotation);
+                Debug.Log("Spawn Egg Ant");
+            }
+
+            elapsedTime = 0f;
+        }
 	}
 
 
